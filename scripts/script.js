bc9fea3e6fe7beeb1c0004b6f628a360fd876973
Anna Musiał <aiwmusial@gmail.com>
2021-03-17 13:57:00 +0100

    Form validation added!

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,21 @@
+//validating form input - name
+function nameCheck(){
+    let fieldInput = document.getElementById("from_name").value;
+    if(fieldInput.length>=2){
+        document.getElementById('name_validator').innerHTML = 'Input accepted!';
+    }else{
+        document.getElementById('name_validator').innerHTML = 'We accept name input of at least two characters!';
+    }
+}
+//validating form input - email
+function emailCheck(){
+    let fieldInput = document.getElementById("from_email").value;
+    if(fieldInput.includes('@')){
+        document.getElementById('email_validator').innerHTML = 'Input accepted!';
+    }else{;
+        document.getElementById('email_validator').innerHTML = 'Seems that you are missing the \'@\' sign!';
+    }
+}
 // function to send messages from the model form
 function sendMail(params){
     let messageBody = {
