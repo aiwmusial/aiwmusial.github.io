6ff07f5ab599acd8b2b32b35b03d96d4f1ef2623
aiwmusial <53020292+aiwmusial@users.noreply.github.com>
2021-07-02 21:15:05 +0200

    DOM updated

--- scripts/script.js
+++ scripts/script.js
@@ -16,13 +16,15 @@ function emailCheck(){
         document.getElementById('email_validator').innerHTML = 'Seems that you are missing the \'@\' sign!';
     }
 }
+
 // function to send messages from the model form
+
 function sendMail(params){
     let messageBody = {
         to_name: 'Anna',
         from_name: document.getElementById("from_name").value,
         from_email: document.getElementById("from_email").value,
-        message: document.getElementById("message").value,
+        message: document.getElementById("body").value,
     };
     emailjs.send('service_w329smh','template_tbg4p7f', messageBody)
     .then(function(res){
