4acdd58bbf57fa7e50e6381ab4f9c7edec655d89
aiwmusial <53020292+aiwmusial@users.noreply.github.com>
2021-07-05 09:57:46 +0200

    js form check updated to include msg check

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,13 @@
+// declaring variables
+let fieldInputName;
+let fieldInputEmail;
+let fieldInputBody;
+let messageBody;
+
 //validating form input: name
 function nameCheck(){
-    let fieldInput = document.getElementById("from_name").value;
-    if(fieldInput.length>=2){
+    fieldInputName = document.getElementById("from_name").value;
+    if(fieldInputName.length >= 2){
         document.getElementById('name_validator').innerHTML = 'Input accepted!';
     }else{
         document.getElementById('name_validator').innerHTML = 'We accept name input of at least two characters!';
@@ -9,26 +15,44 @@ function nameCheck(){
 }
 //validating form input: email
 function emailCheck(){
-    let fieldInput = document.getElementById("from_email").value;
-    if(fieldInput.includes('@')){
+    fieldInputEmail = document.getElementById("from_email").value;
+    if(fieldInputEmail.includes('@')){
         document.getElementById('email_validator').innerHTML = 'Input accepted!';
     }else{;
         document.getElementById('email_validator').innerHTML = 'Seems that you are missing the \'@\' sign!';
+    }
+}
+//validating form input: message
+function messageCheck(){
+    fieldInputBody = document.getElementById("msg_body").value;
+    if(fieldInputBody.length != 0){
+        document.getElementById('msg_validator').innerHTML = 'Input accepted!';
+    }else{;
+        document.getElementById('msg_validator').innerHTML = 'Did you leave any message?';
     }
 }
 
 // function to send messages from the model form
 
 function sendMail(params){
-    let messageBody = {
-        to_name: 'Anna',
-        from_name: document.getElementById("from_name").value,
-        from_email: document.getElementById("from_email").value,
-        message: document.getElementById("msg_body").value
-    };
-    emailjs.send('service_w329smh','template_tbg4p7f', messageBody)
-    .then(function(res){
-        console.log('sucsess',res.status);
-    })
-    
+
+        messageBody = {
+            to_name: 'Anna',
+            from_name: fieldInputName,
+            from_email: fieldInputEmail,
+            message: fieldInputBody
+        };
+
+        if(fieldInputName.length >= 2 && fieldInputEmail.includes('@') && fieldInputBody.length != 0){
+            emailjs.send('service_w329smh','template_tbg4p7f', messageBody)
+            .then(function(res){
+                console.log('sucsess',res.status);
+            })
+            document.getElementsByTagName('form')[0].reset();
+        }else{
+            alert('Did you try to submitt empty or invalid form? Please chack your input!');
+            document.getElementsByTagName('form')[0].reset();
+
+        }
+
 }
