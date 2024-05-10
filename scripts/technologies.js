49c9f0764108538f5605025ff8f18af4bc494f11
Anna Musial <aiwmusial@gmail.com>
2024-05-10 12:31:48 +0200

    skills and technologies part altered + housekeeping

---
+++ scripts/technologies.js
@@ -0,0 +1,74 @@
+const technologies = [
+    {
+        "id":"JavaScript",
+        "description": "Java Script",
+        "source": "./images/techLogo/javascript-logo-png-transparent.png", 
+        "alt": "JavaScript logo"
+    },
+    {
+        "id":"jQuery",
+        "description": "jQuery",
+        "source": "./images/techLogo/jquery-logo-png-png.png", 
+        "alt": "jQuery logo"
+    },
+    {
+        "id":"LogiStudio",
+        "description": "Logi Studio",
+        "source": "./images/techLogo/logi-analytics-logo.png", 
+        "alt": "Logi Analytics logo"
+    },
+    {
+        "id":"PhpStorm",
+        "description": "PhpStorm",
+        "source": "./images/techLogo/PhpStorm_Icon.png", 
+        "alt": "Php Storm logo"
+    },
+    {
+        "id":"pgAdmin",
+        "description": "pgAdmin",
+        "source": "./images/techLogo/PostgreSQL-Logo.wine.png", 
+        "alt": "pgAdmin logo"
+    },
+    {
+        "id":"VisualStudioCode",
+        "description": "Visual Studio Code",
+        "source": "./images/techLogo/vscode.png", 
+        "alt": "Visual Studio Code logo"
+    },
+    {
+        "id":"VueJS",
+        "description": "Vue.js",
+        "source": "./images/techLogo/vuejs.png", 
+        "alt": "Vue.js logo"
+    },
+    {        
+        "id":"HTML5",
+        "description": "HTML 5",
+        "source": "./images/techLogo/html5-logo-png-transparent.png", 
+        "alt": "HTML logo"
+    },
+    {
+        "id":"CSS3",
+        "description": "CSS 3",
+        "source": "./images/techLogo/css3-logo-png-transparent.png", 
+        "alt": "CSS logo"
+    },
+    {        
+        "id":"Bootstrap",
+        "description": "Bootstrap",
+        "source": "./images/techLogo/bootstrap-img.png", 
+        "alt": "bootstrap logo"
+    },
+    {
+        "id":"GitHub",
+        "description": "GitHub",
+        "source": "./images/techLogo/GitHub-Mark-120px-plus.png", 
+        "alt": "GitHub logo"
+    },
+    {
+        "id":"Figma",
+        "description": "Figma",
+        "source": "./images/techLogo/figma-1-logo-png-transparent.png", 
+        "alt": "Figma logo"
+    }
+];
--- scripts/technologiesUsed.js
+++ scripts/technologiesUsed.js
@@ -1,88 +1,27 @@
-let technologies = [
-    {
-        "description": "Java Script",
-        "source": "./images/javascript-logo-png-transparent.png", 
-        "alt": "JavaScript logo"
+// import { technologies } from "./technologies.js"
+const app = Vue.createApp({
+    data() {
+        return {
+            technologies: technologies
+        };
     },
-    {
-        "description": "jQuery",
-        "source": "./images/jquery-logo-png-png.png", 
-        "alt": "jQuery logo"
-    },
-    {
-        "description": "Logi Studio",
-        "source": "./images/logi-analytics-logo.png", 
-        "alt": "Logi Analytics logo"
-    },
-    {
-        "description": "PhpStorm",
-        "source": "./images/PhpStorm_Icon.png", 
-        "alt": "Php Storm logo"
-    },
-    {
-        "description": "pgAdmin",
-        "source": "./images/PostgreSQL-Logo.wine.png", 
-        "alt": "pgAdmin logo"
-    },
-    {
-        "description": "Visual Studio Code",
-        "source": "./images/vscode.png", 
-        "alt": "Visual Studio Code logo"
-    },
-    {
-        "description": "Vue.js",
-        "source": "./images/vuejs.png", 
-        "alt": "Vue.js logo"
-    },
-    {        
-        "description": "HTML 5",
-        "source": "./images/html5-logo-png-transparent.png", 
-        "alt": "HTML logo"
-    },
-    {
-        "description": "CSS 3",
-        "source": "./images/css3-logo-png-transparent.png", 
-        "alt": "CSS logo"
-    },
-    {        
-        "description": "Bootstrap",
-        "source": "./images/bootstrap-img.png", 
-        "alt": "bootstrap logo"
-    },
-    {
-        "description": "GitHub",
-        "source": "./images/GitHub-Mark-120px-plus.png", 
-        "alt": "GitHub logo"
-    },
-    {
-        "description": "Figma",
-        "source": "./images/figma-1-logo-png-transparent.png", 
-        "alt": "Figma logo"
-    }
-];
 
-    let skillsAndTechnologies = document.getElementsByClassName("skills-and-technologies");
-    let skillSetToBeAppended = [];
-
-    for (let i = 0; i < technologies.length; i++){
-        
-        let div = document.createElement("div");
-        let card = document.createElement("div");
-        let cardHeader = document.createElement("div");
-        let cardImage = document.createElement("img");
-
-        div.classList.add("mb-3", "col-lg-2", "col-6", "d-flex", "align-items-center", "justify-content-center")
-        card.className = "card";
-        cardHeader.classList.add("card-header", "text-center", "align-middle");
-        cardImage.classList.add("img-fluid", "p-2");
-
-        div.appendChild(card);
-        card.appendChild(cardHeader);
-        card.appendChild(cardImage);
-
-        cardHeader.innerHTML = `${technologies[i].description}`;
-        cardImage.setAttribute("src",`${technologies[i].source}`);
-        cardImage.setAttribute("alt",`${technologies[i].alt}`);
-        
-        skillsAndTechnologies[0].appendChild(div);
-    }
+    template: ` <div 
+                    v-for="tech in technologies"
+                    :key="tech.id"
+                    class="mb-3 col-lg-2 col-6 d-flex align-items-center justify-content-center"
+                >
+                    <div 
+                        class="card"
+                    >
+                        <div class="card-header text-center align-middle">
+                            {{ tech.description }}
+                        </div>
+                        <img 
+                            class="img-fluid p-2" 
+                            :src="tech.source" 
+                            :alt="tech.alt"
+                        >
+                    </div>
+                </div>`
+}).mount("#skillsAndTech");
